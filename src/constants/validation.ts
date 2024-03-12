165da97cd4fbe99b0050752ebb371f1ac6d750be
NAM Do Van 1 (VTI.D5) <nam.dovan1@vti.vn>
2024-03-12 14:15:21 +0700

    Add ui messenger

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -1,4 +1,8 @@
 const VALIDATION = {
+    EMAIL_FORMAT: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+    PHONE_FORMAT: /^[0-9-]{12,13}$|^[0-9-]{12}$/,
+    ZIP_CODE: /^[0-9]{3}-?[0-9]{4}$/,
+    VALID_TIME: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/g,
     ERROR_CODE: {
         VALIDATE_FAILED: 422
     },
